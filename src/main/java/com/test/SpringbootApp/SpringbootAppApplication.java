fb60888113359e57712db07f13eb0baab40b86e0
한욱 <138234676+hwkimv@users.noreply.github.com>
2025-12-01 21:51:37 +0900

    test 안에 있는 SpringbootAppApplication 주석처리

--- src/main/java/com/test/SpringbootApp/SpringbootAppApplication.java
+++ src/main/java/com/test/SpringbootApp/SpringbootAppApplication.java
@@ -3,7 +3,7 @@ package com.test.SpringbootApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SpringbootAppApplication {
 
 	public static void main(String[] args) {
